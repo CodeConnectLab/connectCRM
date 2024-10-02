ba44444afb5fbc96139f44f8c0ab037d8bdd725c
Pro-Developer01 <yadavshashank70@gmail.com>
2024-10-02 18:04:32 +0530

    "Make 'name' property in InputGroupProps optional"

--- src/components/FormElements/InputGroup/index.tsx
+++ src/components/FormElements/InputGroup/index.tsx
@@ -3,7 +3,7 @@ import React from "react";
 interface InputGroupProps {
   customClasses?: string;
   label: string;
-  name: string;
+  name?: string;
   type: string;
   placeholder: string;
   required?: boolean;
