fa43964e4a850ed5fd02046a02367cb2b44be6fc
Pro-Developer01 <yadavshashank70@gmail.com>
2024-10-20 20:57:27 +0530

    "Added linePattern.css import, updated login page layout and components, and modified signin and login page functionality"

--- src/app/signup/page.tsx
+++ src/app/signup/page.tsx
@@ -3,14 +3,14 @@ import metadataStatic from "@/utils/metadataStatic";
 import LoginPage from "@/components/Pages/Authentication/LoginPage";
 export const metadata: Metadata = metadataStatic;
 
-const LoginPageUI = () => {
+const SignUpPage = () => {
   return (
     <div className="linePattern_Container dark">
       <div className="m-auto flex h-screen max-w-6xl items-center  justify-center">
-        <LoginPage isSignIn={true} />
+        <LoginPage isSignIn={false}/>
       </div>
     </div>
   );
 };
 
-export default LoginPageUI;
+export default SignUpPage;
